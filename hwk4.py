4245c5d5b04a1291845d3ed44580bf934d975cde
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2022-12-04 15:55:11 -0600

    score of 100

--- hwk4.py
+++ hwk4.py
@@ -1862,21 +1862,21 @@ def train_model(model, vocab, train_data_loader, optimizer, n_epochs, device):
 
 if __name__ == "__main__":
     # HYPERPARAMETERS - Feel free to change
-    # BATCH_SIZE = 1024
-    # LEARNING_RATE = 0.01
-    # N_EPOCHS = 10
-    # HIDDEN_SIZE = 300
-    # DROPOUT_PROB = 0.1
-    # EMBED_SIZE = 100
-    # WEIGHT_DECAY = 1e-8
-
     BATCH_SIZE = 1024
-    LEARNING_RATE = 0.03
-    N_EPOCHS = 20
-    HIDDEN_SIZE = 600
+    LEARNING_RATE = 0.01
+    N_EPOCHS = 10
+    HIDDEN_SIZE = 300
     DROPOUT_PROB = 0.1
-    EMBED_SIZE = 200
+    EMBED_SIZE = 100
     WEIGHT_DECAY = 1e-8
+
+    # BATCH_SIZE = 1024
+    # LEARNING_RATE = 0.03
+    # N_EPOCHS = 20
+    # HIDDEN_SIZE = 600
+    # DROPOUT_PROB = 0.1
+    # EMBED_SIZE = 200
+    # WEIGHT_DECAY = 1e-8
 
     N_EMBEDDINGS = vocab.n_tokens # Do not change!
     N_FEATURES = 48 # Do not change!
@@ -1920,7 +1920,7 @@ if __name__=='__main__':
 # 
 # This function is worth <b>8 points</b>, and there is no partial credit.
 
-# In[48]:
+# In[55]:
 
 
 def select_best_legal_action(parser_configs, predictions, n_labels):
@@ -2019,9 +2019,17 @@ def select_best_legal_action(parser_configs, predictions, n_labels):
             bitmap_1D_np_array[2*n_labels] = 1
             # bitmap_1D_np_array[:2*n_labels] = 0
 
+        # LA
+        # if (len(stack) >= 3) or (len(buffer) == 0 and len(stack) >= 2):
+        if len(stack) >= 3:
+            # print(f"LA-label is a valid action for this parser config")
+            # bitmap_1D_np_array[0:n_labels-1] = 1
+            bitmap_1D_np_array[0:n_labels] = 1
+            # bitmap_1D_np_array[n_labels:] = 0
+
         # RA
-        if (len(stack) >= 3) or (len(buffer) == 0 and len(stack) == 2):
-        # if len(stack) >= 3:
+        # if (len(stack) >= 3) or (len(buffer) == 0 and len(stack) >= 2):
+        if len(stack) >= 2:
             # print(f"RA-label is a valid action for this parser config")
             # bitmap_1D_np_array[0:n_labels] = 0
             # bitmap_1D_np_array[n_labels:2*n_labels-1] = 1
@@ -2029,15 +2037,10 @@ def select_best_legal_action(parser_configs, predictions, n_labels):
             # bitmap_1D_np_array[2*n_labels] = 0
 
 
-        # LA
-        if (len(stack) >= 3) or (len(buffer) == 0 and len(stack) == 2):
-        # if len(stack) >= 3:
-            # print(f"LA-label is a valid action for this parser config")
-            # bitmap_1D_np_array[0:n_labels-1] = 1
-            bitmap_1D_np_array[0:n_labels] = 1
-            # bitmap_1D_np_array[n_labels:] = 0
 
-        predictions[index] = predictions[index] + (bitmap_1D_np_array*100)
+
+        # predictions[index] = predictions[index] + (bitmap_1D_np_array*100)
+        predictions[index] *= bitmap_1D_np_array
 
         # #############################################
         # # with 2D bitmap
@@ -2105,7 +2108,7 @@ def select_best_legal_action(parser_configs, predictions, n_labels):
 
 # Now we provide you with a function that takes a (trained) model and makes the best legal prediction for a batch of parser configurations. You do <b>not</b> need to edit this cell.
 
-# In[49]:
+# In[56]:
 
 
 ### DO NOT EDIT ###
@@ -2137,7 +2140,7 @@ def predict(model, vocab, parser_configs):
 # 
 # You do <b>not</b> need to edit this cell.
 
-# In[50]:
+# In[57]:
 
 
 ### DO NOT EDIT ###
@@ -2225,7 +2228,7 @@ def evaluate(model, vocab, dataset, eval_batch_size=5000):
 
 # Run the following cell to calculate your attachment scores. You must achieve a <b>labeled attachment score</b> of <b>≥ 80%</b> for full credit. Bear in mind that Gradescope uses a different (hidden) test set, so results may be slightly different.
 
-# In[51]:
+# In[58]:
 
 
 ### DO NOT EDIT ###
@@ -2245,7 +2248,7 @@ if __name__=="__main__":
 # * ⍻: Edges for which you predicted the <b>correct head but incorrect label</b>
 # * ×: Edges that you do not have in your tree (i.e., you predicted the <b>incorrect head<b>).
 
-# In[52]:
+# In[59]:
 
 
 ### DO NOT EDIT ###
@@ -2285,7 +2288,7 @@ def diagnose_sentences(idxes, data, preds, min_len, max_len, num_to_print=5):
         print('-'*100, '\n')
 
 
-# In[53]:
+# In[60]:
 
 
 if __name__== '__main__':
@@ -2304,7 +2307,7 @@ if __name__== '__main__':
 # 1.   `hwk4.py`, the download of this notebook as a `.py` file (**not** a `.ipynb` file)
 # 1.   `model.pt`, the saved version of your `model`
 
-# In[54]:
+# In[61]:
 
 
 ### DO NOT EDIT ###
@@ -2321,13 +2324,25 @@ if __name__=='__main__':
     print("Saved!")
 
 
-# In[54]:
+# In[61]:
 
 
 
 
 
-# In[54]:
+# In[61]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[61]:
 
 
 
